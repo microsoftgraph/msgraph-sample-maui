e1a5c8ac66e9d9489ee3f9c039ee826db20b4841
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-01-28 10:52:16 -0500

    Basic UI

---
+++ demos/01-create-app/GraphTutorial/GraphTutorial/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace GraphTutorial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SignInPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        public static void SignIn()
+        {
+
+        }
+
+        public static void SignOut()
+        {
+
+        }
+    }
+}
---
+++ demos/01-create-app/GraphTutorial/GraphTutorial/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GraphTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            lblUserName.Text = "Test User";
+            lblUserEmail.Text = "test@contoso.com";
+
+            imgProfilePhoto.Source = ImageSource.FromStream(() => GetUserPhoto());
+        }
+
+        private Stream GetUserPhoto()
+        {
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream("GraphTutorial.no-profile-pic.png");
+        }
+
+        private async void OnUserTapped(object sender, EventArgs e)
+        {
+            var signout = await DisplayAlert("Sign out?", "Do you want to sign out?", "Yes", "No");
+            if (signout)
+            {
+                App.SignOut();
+
+                await Navigation.PushModalAsync(new NavigationPage(new SignInPage()), true);
+            }
+        }
+    }
+}
