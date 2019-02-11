e6846f1a38fc9ead8fc836a5ca7aadbf087f286b
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-02-11 14:22:32 -0500

    JSON dump of events

--- demos/03-add-msgraph/GraphTutorial/GraphTutorial/CalendarPage.xaml.cs
+++ demos/03-add-msgraph/GraphTutorial/GraphTutorial/CalendarPage.xaml.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace GraphTutorial
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CalendarPage : ContentPage
-	{
-		public CalendarPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalendarPage : ContentPage
+    {
+        public CalendarPage ()
+        {
+            InitializeComponent ();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Get the events
+            var events = await App.GraphClient.Me.Events.Request()
+                .Select("subject,organizer,start,end")
+                .OrderBy("createdDateTime DESC")
+                .GetAsync();
+
+            // Temporary
+            JSONResponse.Text = JsonConvert.SerializeObject(events.CurrentPage, Formatting.Indented);
+        }
+    }
 }
