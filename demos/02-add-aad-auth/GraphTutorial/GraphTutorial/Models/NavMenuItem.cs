7c6e2ac7d57a033765089563ce40f6e687d97b38
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-05-24 13:05:21 -0400

    Copied completed 01 project to 02 folder

---
+++ demos/02-add-aad-auth/GraphTutorial/GraphTutorial/Models/NavMenuItem.cs
@@ -0,0 +1,15 @@
+﻿namespace GraphTutorial.Models
+{
+    public enum MenuItemType
+    {
+        Welcome,
+        Calendar
+    }
+
+    public class NavMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ demos/02-add-aad-auth/GraphTutorial/GraphTutorial/WelcomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GraphTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSignIn(object sender, EventArgs e)
+        {
+            (App.Current.MainPage as MasterDetailPage).IsPresented = true;
+        }
+    }
+}
