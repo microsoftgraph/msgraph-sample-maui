45b531ccb3ca5f90a442f9c1ba9eade1ebd25e58
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-04-01 17:05:47 -0400

    Completed the add MS Graph step

--- demo/GraphTutorial/GraphTutorial/CalendarPage.xaml.cs
+++ demo/GraphTutorial/GraphTutorial/CalendarPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Graph;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +19,26 @@ namespace GraphTutorial
         public CalendarPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Get the events
+            var events = await App.GraphClient.Me.Events.Request()
+                .Select(e => new 
+                { 
+                    e.Subject, 
+                    e.Organizer, 
+                    e.Start, 
+                    e.End 
+                })
+                .OrderBy("createdDateTime DESC")
+                .GetAsync();
+
+            // Add the events to the list view
+            CalendarList.ItemsSource = events.CurrentPage.ToList();
         }
     }
 }
---
+++ demo/GraphTutorial/GraphTutorial/Models/GraphDateTimeTimeZoneConverter.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// <DateTimeConverterSnippet>
+using Microsoft.Graph;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace GraphTutorial.Models
+{
+    class GraphDateTimeTimeZoneConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTimeTimeZone date)
+            {
+                // Resolve the time zone
+                var timezone = TimeZoneInfo.FindSystemTimeZoneById(date.TimeZone);
+                // Parse method assumes local time, which may not be the case
+                var parsedDateAsLocal = DateTimeOffset.Parse(date.DateTime);
+                // Determine the offset from UTC time for the specific date
+                // Making this call adjusts for DST as appropriate
+                var tzOffset = timezone.GetUtcOffset(parsedDateAsLocal.DateTime);
+                // Create a new DateTimeOffset with the specific offset from UTC
+                var correctedDate = new DateTimeOffset(parsedDateAsLocal.DateTime, tzOffset);
+                // Return the local date time string
+                return $"{correctedDate.LocalDateTime.ToShortDateString()} {correctedDate.LocalDateTime.ToShortTimeString()}";
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+// </DateTimeConverterSnippet>
