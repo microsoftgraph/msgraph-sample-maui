79cfabfae0a4448360a956f0c7bdedf51f463778
Jason Johnston <jasonjoh@users.noreply.github.com>
2024-12-12 14:12:13 -0500

    Delete old project

--- GraphMAUI/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace GraphMAUI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
